1f2c251ee4159314871f7e85ab09bc29c146ef92
Anuradha Shettigar <ashettigar01@gmail.com>
2025-12-30 15:38:03 +0530

    Update siteDetails with new metadata and empty values

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -1,12 +1,12 @@
 export const siteDetails = {
-    siteName: 'Finwise',
-    siteUrl: 'https://finwise-omega.vercel.app/',
+    siteName: '',
+    siteUrl: '',
     metadata: {
-        title: 'Finwise - Next.js and Tailwind CSS Landing Page Template',
-        description: 'Finwise empowers businesses with cutting-edge technology solutions to drive success and efficiency.',
+        title: 'Landing Page Assignment',
+        description: 'Professional landing page built using Next.js.',
     },
     language: 'en-us',
     locale: 'en-US',
-    siteLogo: `${process.env.BASE_PATH || ''}/images/logo.png`, // or use a string for the logo e.g. "TechStartup"
-    googleAnalyticsId: '', // e.g. G-XXXXXXX,
+    siteLogo: '',
+    googleAnalyticsId: '',
 }
